6a18357124c49f2c2bdedd92bc6679b40400dd37
Hala-sherif98 <halasherif714@gmail.com>
2023-03-12 19:30:40 +0200

    Add project files.

---
+++ AspLabDay1/Controllers/CarsController.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using AspLabDay1.Models;
+
+
+namespace AspLabDay1.Controllers
+{
+
+    public enum Type
+    {
+        list,
+        table
+    }
+    public class CarsController : Controller
+    {
+        public IActionResult GetAll()
+        {
+            var cars = Car.GetCars();
+            return View(cars);
+            
+        }
+        public IActionResult GetDetails(string Carmodel, Type? type )
+        {
+            var car = Car.GetCars().First(c=> c.Model == Carmodel);
+            return View(car);
+
+        }
+    }
+}
